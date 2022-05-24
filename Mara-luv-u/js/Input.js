f691c6290ecd88f40b356e7702ec281915d90972
Hakiraa <Jblbermuda@web.de>
2022-05-24 14:05:06 +0200

    maras-webside-beta1

---
+++ Mara-luv-u/js/Input.js
@@ -0,0 +1,87 @@
+let send = document.querySelector('#send')
+let nput = document.querySelector('#get-in')
+let output = document.getElementById("link")
+
+function returnText(){
+    let input = document.getElementById("get-in").value;
+    switch(input){
+        case "1AdD":
+        output.href="1es-Jahr.html" 
+        output.innerHTML = "Klick mich für das erste Jahr" 
+        output.style.textDecoration = "underline"
+        console.log("a") 
+
+        case "2DfA":
+        break;
+
+
+        case "3sDf":
+        break;
+
+
+        case "4jAk":
+        break;
+
+
+        case "5AdF":
+        break;
+
+
+        case "6AsS":
+        break;
+
+
+        case "7aAd":
+        break;
+
+
+        case "8fRa":
+        break;
+
+
+        case "9asD":
+        break;
+
+
+        case "10Sf":
+        break;
+
+
+        case "11Da":
+        break;
+
+
+        case "12dA":
+        break;
+
+
+        case "13gJ":
+        break;
+
+
+        case "14ds":
+        break;
+
+
+        case "15Ad":
+        break;
+
+
+        case "16Sf":
+        break;
+
+
+        case "17sN":
+        break;
+
+
+        case "18Ad":
+        break;
+
+        
+        default:
+            output.href="eingabe.html" 
+            output.innerHTML = "Da hast du dich wohl vertippt"
+        break;
+    }
+}
