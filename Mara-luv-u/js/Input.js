04a111fec42918875a0552362c05e89c56fd481c
Hakiraa <106157908+Hakiraa@users.noreply.github.com>
2022-05-24 19:05:58 +0200

    update

--- Mara-luv-u/js/Input.js
+++ Mara-luv-u/js/Input.js
@@ -10,22 +10,18 @@ function returnText(){
         output.innerHTML = "Klick mich für das erste Jahr" 
         output.style.textDecoration = "underline"
         break;
-        
+
         case "2Dfa":
         output.href="2es-Jahr.html" 
         output.innerHTML = "Klick mich für das zweite Jahr" 
         output.style.textDecoration = "underline"
-
         break;
+        
         case "3sDf":
             output.href="3es-Jahr.html" 
             output.innerHTML = "Klick mich für das dritte Jahr" 
             output.style.textDecoration = "underline"
-    
-            
         break;
-
-
         case "4jAk":
             output.href="4es-Jahr.html" 
             output.innerHTML = "Klick mich für das Vierte Jahr" 
@@ -34,58 +30,100 @@ function returnText(){
 
 
         case "5AdF":
+            output.href="5es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Fünfte Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
 
         case "6AsS":
+            output.href="6es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Sechse Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
 
         case "7aAd":
+        output.href="7es-Jahr.html" 
+        output.innerHTML = "Klick mich für das Siebte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "8fRa":
+        output.href="8es-Jahr.html" 
+        output.innerHTML = "Klick mich für das achte Jahr (Von Quinn (die kleine hure))" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "9asD":
+        output.href="9es-Jahr.html" 
+        output.innerHTML = "Klick mich für das neunte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "10Sf":
+        output.href="https://youtu.be/RMx90twCZ54" 
+        output.innerHTML = "Klick mich für das Zehnte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "11Da":
+        output.href="11es-Jahr.html" 
+        output.innerHTML = "Klick mich für das Elfte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "12dA":
+        output.href="12es-Jahr.html" 
+        output.innerHTML = "Klick mich für das Zwölfte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "13gJ":
+        output.href="13es-Jahr.html" 
+        output.innerHTML = "Klick mich für das Dreizehnte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "14ds":
+        output.href="14es-Jahr.html" 
+        output.innerHTML = "Klick mich für das Vierzehnte Jahr" 
+        output.style.textDecoration = "underline"
         break;
 
 
         case "15Ad":
+            output.href="15es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Fünfzehnte Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
 
         case "16Sf":
+            output.href="16es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Sechszehnte Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
 
         case "17sN":
+            output.href="17es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Siebzehnte Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
 
-        case "18Ad":
+        case "18AddsafD":
+            output.href="18es-Jahr.html" 
+            output.innerHTML = "Klick mich für das Achtzehnte und letzte Jahr" 
+            output.style.textDecoration = "underline"
         break;
 
         
