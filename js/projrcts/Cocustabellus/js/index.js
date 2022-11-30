7ece3c22d3aa1ff39cd3cd9b7f9a3d511583acc1
Hakiraa <106157908+Hakiraa@users.noreply.github.com>
2022-11-30 10:32:52 +0100

    asdf

---
+++ js/projrcts/Cocustabellus/js/index.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+
+var output = fs.readFileSync('data.txt')
+let Entry = document.getElementById('entry');
+
+
+const data_get = function(dataT, name){
+    
+    return output;
+
+};
+console.log(readFile('Input.txt', 'utf-8',))
+
+const keydetect = function(ent){
+    if (ent == keyword) {
+        return true;
+    } else {
+        return false;
+    }
+};
+console.log(data_get())
