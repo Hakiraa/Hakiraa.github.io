c6cec8a458bbfc0aa0f4fd7931e58208d0294fa5
Hakiraa <106157908+Hakiraa@users.noreply.github.com>
2022-12-01 11:17:03 +0100

    qwe

--- js/projrcts/yt/Codetrain/APIs in js/2.1/index.js
+++ js/projrcts/yt/Codetrain/APIs in js/2.1/index.js
@@ -1,12 +1,36 @@
 const express = require('express');
+const fs = require('fs');
+const Datastore = require('nedb');
 const app = express();
 const port = 3000
+let savedata = []
 app.listen(port, () => console.log("listening on port " + port));
 app.use(express.static("public"));
+
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+
 
 app.use(express.json({limit: "1mb"}));
 
 
 app.post('/api', (req, res) => {
-    console.log(req);
+    const data = req.body
+    data.timestamp = Date.now();
+    database.insert(data);
+    res.json({  
+        status: "Success",
+        data: req.body,
+        tim: data.timestamp
+     });
+});
+app.get("/api", (req, res) => {
+    database.find({},(err, data) => {
+        if (err) {
+            res.end();
+            return;
+        }
+        res.json({data});
+    });
 });
