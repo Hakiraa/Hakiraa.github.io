11aa24fff9df3ac9aa9d313a841d83d37b2f5b9d
Hakiraa <jblbermuda@web.de>
2023-01-28 22:08:31 +0100

    Dada

---
+++ index.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body") 
+const firstCard = document.getElementById('firstcard');
+firstCard.addEventListener("click", () => {
+    firstCard.style.width = "98vmin";
+    firstCard.style.height = "98vh";
+    firstCard.style.transform = "none";
+    firstCard.style.transform = "rotate(0deg) translateX(-0%)"
+    firstCard.classList.remove("inactive")
+    
+
+
+
+})
